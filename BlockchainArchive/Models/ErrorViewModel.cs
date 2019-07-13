e0dfca7891a95d0bee604761d299bc3a4b696094
lukasz <lukasz.jakubczak1@gmail.com>
2019-07-13 13:41:59 +0200

    Initial commit

---
+++ BlockchainArchive/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlockchainArchive.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
